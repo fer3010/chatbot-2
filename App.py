58fd0c0447551cdc181de172088a5571ce645d53
fer3010 <fernando27926549@gmail.com>
2025-08-26 15:40:05 +0000

    Corregir formato y agregar rutas para la respuesta del bot en App.py

--- App.py
+++ App.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template,request,jsonify
+from flask import Flask, render_template, request, jsonify
 import requests
-import json
 import os
 
-app = Flask(__name__)
-API_KEY= os.environ.get('API_KEY')
-API_URL= ""
+app= Flask (__name__)
+API_KEY = os.environ.get('API_KEY')
+API_URL=
+.env 
+API_KEY=
+@app.route('/')
+def home():
+    return render_template("index.html")
+@app.route("/get",methods=['POST'])
+def get_bot_response_route():
+    user_data=request.get_json()
+    user_text=user_data.get('msg')
+    if not user_text:
+        return jsonify({"error": "No hay mensaje"}), 400
+    payload = {
+        "inputs": user_text
+    }
